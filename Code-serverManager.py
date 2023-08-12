c212656d4437cc765aa4843b0efc621585e58097
“XiaoJiang0208” <jjm1837080396@gmail.com>
2023-08-12 17:54:34 +0800

    改

--- Code-serverManager.py
+++ Code-serverManager.py
@@ -1,22 +1,37 @@
 from flask import *
 #import sqlite3
-webapp = Flask(__name__, static_url_path='', static_folder='templates', template_folder='templates')
-
+webapp = Flask(__name__)
 
 
 #webapp
-@webapp.route('/login/')
+#登录系统
+@webapp.route('/login/', methods=['GET', 'POST'])
 def login():
-    return render_template('login.html')
+    if request.method=='GET':
+        return render_template('login.html')
+    elif request.method=='POST':
+        print(request.form.get('username'))
+        return redirect(url_for('login'))
 
-@webapp.route("/api/login/",methods=['POST'])
-def apilogin():
-    print(request.form.get('username'))
-    return redirect(url_for('login'))
+#注册系统
+@webapp.route('/signup/')
+def rtsignup():
+    return redirect('/signup/welcome')
+@webapp.route('/signup/<msg>/', methods=['GET', 'POST'])
+def signup(msg):
+    if request.method=='GET':
+        return render_template('signup.html',msg=msg)
+    elif request.method=='POST':
+        if request.form.get('password')!=request.form.get('confpassword'):
+            return redirect('/login/密码不匹配!')
+        return redirect(url_for('login'))
+   
 
-@webapp.route('/signup/<msg>')
-def test(msg):
-    return render_template('signup.html',msg=msg)
-    
+def adduser(username,password,pwoer):
+    with open('./config/userdata.json','r') as f:
+        data=json.load(f)
+
+
 if __name__ == "__main__":
     webapp.run(host='0.0.0.0',port=5000,debug=True)
+    adduser("1","1","1")
--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # Code-serverManager
+为code-server实现了多用户和管理
+
+# 本项目在早期开发阶段
+# 待实现功能
+> 数据库存储数据
+> ...
