b84adce0f79b24efdbf7081c96a9ea5ceec5245c
“XiaoJiang0208” <jjm1837080396@gmail.com>
2023-08-13 02:07:32 +0800

    最最最基础注册逻辑

--- Code-serverManager.py
+++ Code-serverManager.py
@@ -20,16 +20,23 @@ def signup():
     if request.method=='GET':
         return render_template('signup.html',msg="")
     elif request.method=='POST':
-        if request.form.get('password')!=request.form.get('confpassword'):
-            return render_template('signup.html',msg="密码不匹配！")
+        if adduser(request.form.get('username'),request.form.get('password'),'admin'):
+            return render_template('signup.html',msg='成功!')
+        return render_template('signup.html',msg='失败!')
         
    
 
 def adduser(username,password,pwoer):
     with open('./config/userdata.json','r') as f:
         data=json.load(f)
+    for i in data['users']:
+        if i['username']==username:
+            return 0
+    data['users'].append({'username':username,'password':password,'pwoer':pwoer,'token':''})
+    with open('./config/userdata.json','w') as f:
+        json.dump(data,f)
+    return 1
 
 
 if __name__ == "__main__":
     webapp.run(host='0.0.0.0',port=5000,debug=True)
-    adduser("1","1","1")
